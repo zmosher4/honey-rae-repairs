040753ddf7a313cebe7a3fab36ffc57c0d779d30
Zach Mosher <zmosher4@gmail.com>
2024-07-11 15:37:02 -0500

    started with authentication

--- src/views/ApplicationViews.jsx
+++ src/views/ApplicationViews.jsx
@@ -1,3 +1,44 @@
+import { Outlet, Route, Routes, useSearchParams } from 'react-router-dom';
+import { TicketList } from '../components/tickets/TicketList';
+import { Welcome } from '../components/welcome/Welcome';
+import { Employees } from '../components/Employees/Employees';
+import { EmployeeDetails } from '../components/Employees/EmployeeDetails';
+import { CustomerList } from '../components/customers/CustomersList';
+import { CustomerDetails } from '../components/customers/CustomerDetails';
+import { NavBar } from '../components/nav/NavBar';
+import { useEffect, useState } from 'react';
+
 export const ApplicationViews = () => {
-  return <></>
-}
+  const [currentUser, setCurrentUser] = useState({});
+
+  useEffect(() => {
+    const localHoneyUser = localStorage.getItem('honey_user');
+    const honeyUserObject = JSON.parse(localHoneyUser);
+    setCurrentUser(honeyUserObject);
+  }, []);
+
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <NavBar />
+            <Outlet />
+          </>
+        }
+      >
+        <Route index element={<Welcome />} />
+        <Route path="tickets" element={<TicketList />} />
+        <Route path="employees">
+          <Route index element={<Employees />} />
+          <Route path=":employeeId" element={<EmployeeDetails />} />
+        </Route>
+        <Route path="customers">
+          <Route index element={<CustomerList />} />
+          <Route path=":customerId" element={<CustomerDetails />} />
+        </Route>
+      </Route>
+    </Routes>
+  );
+};
